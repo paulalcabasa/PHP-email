a37b899ef2811ba4df643caa22980431b5c95683
Paul Alcabasa <paulalcabasa@gmail.com>
2019-11-12 13:22:42 +0800

    Update index.php
    updated index file

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <meta name="msapplication-tap-highlight" content="no">
     <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
     <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
-    <title>Complete Email Verification Code</title>
+    <title>Complete Email Verification Code | Paul Alcabasa</title>
 
 
      <?php include 'components/css.php';?>
